688c046303d4262aaae063eb6a3b85a4ea7b5be2
John obiko <obi22004@byui.edu>
2023-11-04 23:48:09 +0300

    hm gj

--- getdates.js
+++ getdates.js
@@ -1,2 +1,36 @@
+// JavaScript for the "Back to Top" button
+const backToTopButton = document.getElementById('back-to-top-button');
 
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
+
+// JavaScript for the hamburger menu
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const navMenu = document.querySelector('nav ul');
+
+hamburgerIcon.addEventListener('click', () => {
+    hamburgerIcon.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+function hideMenuOnLargeScreen() {
+    if (window.innerWidth >= 768) {
+        hamburgerIcon.classList.remove('active');
+        navMenu.classList.remove('active');
+    }
+}
+
+hideMenuOnLargeScreen();
+
+window.addEventListener('resize', hideMenuOnLargeScreen);
 
